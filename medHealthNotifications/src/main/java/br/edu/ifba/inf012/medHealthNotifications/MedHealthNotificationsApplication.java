2ee47ceade4dbb6603a320d004c3f47d1ab6e458
Andrey Gomes <andreygdsn@gmail.com>
2026-01-19 14:10:59 -0300

    feat: initial mail micro service configurations

---
+++ medHealthNotifications/src/main/java/br/edu/ifba/inf012/medHealthNotifications/MedHealthNotificationsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.inf012.medHealthNotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedHealthNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedHealthNotificationsApplication.class, args);
+	}
+
+}
---
+++ medHealthNotifications/src/test/java/br/edu/ifba/inf012/medHealthNotifications/MedHealthNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.inf012.medHealthNotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedHealthNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
